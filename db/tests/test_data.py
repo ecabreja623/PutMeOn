8f0bffe00b6d8a02f09f193ea86f2642272a1a1c
ecabreja623 <ec3539@nyu.edu>
2021-11-30 20:22:33 -0500

    Added common makefile

---
+++ db/tests/test_data.py
@@ -0,0 +1,39 @@
+"""
+This file holds the tests for data.py
+"""
+
+from unittest import TestCase, skip
+
+import data as db
+
+FAKE_USER = "Fake user"
+
+
+class DBTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_write_collection(self):
+        """
+        Can we write the user db?
+        """
+        fake_data = {FAKE_USER: {
+            "numFriends" : 0,
+            "friends" : [],
+            "numPlaylists" : 0,
+            "playlists" : []
+            }
+        }
+        db.write_collection(db.USER_COLLECTION, fake_data)
+        users = db.get_users()
+        self.assertIn(FAKE_USER, users)
+
+    def test_get_users(self):
+        """
+        Can we fetch user db?
+        """
+        users = db.get_users()
+        self.assertIsInstance(users, dict)
